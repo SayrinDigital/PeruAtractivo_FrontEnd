7d4e8e056877c31e1f380ebaccf01ae4575774c1
josepuma <josepuma@sayrin.cl>
2019-11-20 13:03:22 -0300

    Update ItemsGroup.vue

--- components/prismic/page/ItemsGroup.vue
+++ components/prismic/page/ItemsGroup.vue
@@ -2,14 +2,15 @@
 
   section.uk-section
     .uk-container
-      h2.uk-text-capitalize(class="uk-width-large@s") {{ $prismic.asText(content.primary.titulo) }}
-      p.desc(class="uk-width-large@s") {{ $prismic.asText(content.primary.descripcion) }}
-      .uk-margin-top(class="uk-child-width-1-3@m", uk-grid)
-       div(v-if="$prismic.asLink(item.link)", v-for="item in content.items")
-        div
-          nuxt-link.uk-display-block( :to="$prismic.asLink(item.link)")
+      h2.uk-text-capitalize(class="uk-text-center") {{ $prismic.asText(content.primary.titulo) }}
+      p.desc(class="uk-text-center") {{ $prismic.asText(content.primary.descripcion) }}
+      .uk-margin-top(class="uk-child-width-1-4@m", uk-grid)
+       div( v-for="item in content.items")
+        div.uk-text-center
+          nuxt-link.uk-display-block( v-if="$prismic.asLink(item.link)", :to="$prismic.asLink(item.link)")
             img(v-if="item.cover", :src="$prismic.asLink(item.cover)")
-          .uk-margin-top
+          img(v-else, :src="$prismic.asLink(item.cover)")
+          .uk-margin-top.uk-text-center
            h4.uk-text-capitalize.uk-margin-remove {{ $prismic.asText(item.titulo) }}
            p.uk-margin-remove.desc {{ $prismic.asText(item.descripcion) }}
 

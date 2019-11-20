b704cfdb4ffde87ef22307a2aae292c476d58d2a
josepuma <josepuma@sayrin.cl>
2019-11-20 12:50:26 -0300

    Update SectionImageContent.vue

--- components/prismic/SectionImageContent.vue
+++ components/prismic/SectionImageContent.vue
@@ -1,18 +1,22 @@
 <template lang="pug">
 
   section.uk-section.uk-section-large(v-if="content.primary")
-    .uk-flex.uk-flex-middle(uk-grid)
-      div(class="uk-width-2-5@s")
-        div
-          div(v-bind:class="{ 'say-padding-left-container' : content.primary.seccion_alineacion == 'derecha' }")
-            .uk-container(v-if="content.primary")
-                p.head.uk-margin-remove {{ $prismic.asText(content.primary.seccion_subtitulo) }}
-                h2.uk-margin-remove-top(class="uk-width-large@s") {{ $prismic.asText(content.primary.seccion_encabezado) }}
-                p.desc(class="uk-width-large@s") {{ $prismic.asText(content.primary.seccion_contenido) }}
-                nuxt-link.dark-button(:to="$prismic.asLink(content.primary.seccion_link)", v-if="$prismic.asLink(content.primary.seccion_link)") {{ $prismic.asText(content.primary.seccion_link_texto) }} #[span.uk-margin-small-left(uk-icon="minus")]
-      div(class="uk-width-3-5@s", v-bind:class="{ 'uk-flex-first' : content.primary.seccion_alineacion == 'izquierda' }")
-        div
-          img.uk-width-1-1(:src="$prismic.asLink(content.primary.seccion_imagen)")
+    .uk-container
+      .uk-flex.uk-flex-middle(uk-grid)
+        div(class="uk-width-1-2@s")
+          div
+            div
+              div(v-if="content.primary")
+                  p.head.uk-margin-remove {{ $prismic.asText(content.primary.seccion_subtitulo) }}
+                  h2.uk-margin-remove-top(class="uk-width-large@s") {{ $prismic.asText(content.primary.seccion_encabezado) }}
+                  p.desc {{ $prismic.asText(content.primary.seccion_contenido) }}
+                  nuxt-link.dark-button(:to="$prismic.asLink(content.primary.seccion_link)", v-if="$prismic.asLink(content.primary.seccion_link)") {{ $prismic.asText(content.primary.seccion_link_texto) }} #[span.uk-margin-small-left(uk-icon="minus")]
+        div(class="uk-width-1-2@s", v-bind:class="{ 'uk-flex-first' : content.primary.seccion_alineacion == 'izquierda' }")
+          div(uk-slider)
+            ul.uk-slider-items.uk-grid(class="uk-child-width-1-1")
+              li(v-for="(item, index) in content.items", :key="'section-image-container-' + index")
+                img.uk-width-1-1(:src="$prismic.asLink(item.image)")
+            .uk-slider-nav.uk-dotnav.uk-flex-center.uk-margin
 
 </template>
 
